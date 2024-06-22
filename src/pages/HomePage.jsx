d6579d2e1cc0836046dd3d429038e12e14f43c17
Fernando <ing.fernandovg@gmail.com>
2024-06-22 10:53:08 -0600

    add rayquaza moving background

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import { setTrainer } from '../store/slices/trainer.slice';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import pokeimg from '../assets/pokedex-logo.png';
+import rayquaza from '../assets/rayquaza.png';
 import './styles/homepage.css'; 
 
 const HomePage = () => {
@@ -22,15 +23,18 @@ const HomePage = () => {
   return (
     <div className='homepage'>
       <div className="homepage__container">
-        <figure className='homepage__title'>
-          <img src={pokeimg} alt="pokedex" />
-        </figure>
-        <h2 className='homepage__hello'>Welcome Trainer!</h2>
-        <p className='homepage__presentation'>Your adventure is begining! <br /> to start, give me your name: </p>
-        <form onSubmit={handleSubmit} className='homepage__form'>
-          <input type="text" ref={textInput} className='homepage__input' />
-          <button className='homepage__btn'>START</button>
-        </form>
+        <img src={rayquaza} alt="background-image" className='homepage__bg' />
+        <div className="homepage__content">
+          <figure className='homepage__title'>
+            <img src={pokeimg} alt="pokedex" />
+          </figure>
+          <h2 className='homepage__hello'>Welcome Trainer!</h2>
+          <p className='homepage__presentation'>Your adventure is begining! <br /> to start, give me your name: </p>
+          <form onSubmit={handleSubmit} className='homepage__form'>
+            <input type="text" ref={textInput} className='homepage__input' />
+            <button className='homepage__btn'>START</button>
+          </form>
+        </div>
       </div>
     </div>
 
