560d73e5f80204e47b56e771e08afd6a374a4fa2
Fernando <ing.fernandovg@gmail.com>
2024-06-24 14:55:12 -0600

    add navigate logo

--- src/components/shared/Pokeheader.jsx
+++ src/components/shared/Pokeheader.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './styles/pokeheader.css';
 import pokelogo from '../../assets/pokedex-logo.png'
+import { useNavigate } from 'react-router-dom';
 
 const Pokeheader = () => {
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/pokedex');
+  }
+
   return (
     <div className='pokeheader'>
       <div className="pokeheader__red">
         <figure className='pokeheader__img'>
-          <img src={pokelogo} alt="" />
+          <img src={pokelogo} alt="" onClick={handleClick}/>
         </figure>
       </div>
       <div className="pokeheader__black">
